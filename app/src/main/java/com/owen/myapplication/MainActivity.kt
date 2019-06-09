45a9a3e66d1ad490a65655c819ae5286b65c137c
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-06-09 10:53:47 -0400

    Update MainActivity.kt
    added Json function to collect the DAta from the URL

--- app/src/main/java/com/owen/myapplication/MainActivity.kt
+++ app/src/main/java/com/owen/myapplication/MainActivity.kt
@@ -2,6 +2,11 @@ package com.owen.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +16,37 @@ class MainActivity : AppCompatActivity() {
         
         //check if the recyclew accipies the whole parent
         //recycler_view.setBackgroundColor(Color.BLUE)
+        //recycler_view.setBackgroundColor(Color.BLUE)
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        //set a dapter
+        recycler_view.adapter = HomeAdapter()
+
+        //function to collect json files
+        fetchJson()
+    }
+    
+    
+    fun fetchJson()
+    {
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+
+        val request = Request.Builder().url(url).build()
+
+       val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object : Callback{
+
+            override fun onResponse(call: Call, response: Response)
+            {
+                val body = response?.body?.string()
+
+                println(body)
+
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+               println("Failed to Fetch data , try again")
+            }
+        })
     }
 }
